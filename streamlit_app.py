c9921b1f7089d56f952c5bfa0db2488eebfda2dd
noliebrent <112365545+noliebrent@users.noreply.github.com>
2023-05-26 14:53:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,46 +8,40 @@ voc_size = 5000
 max_sent_length = 20
 
 @st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('cnnmodel.hdf5')
+def load_model(model_type):
+    if model_type == 'Model 1':
+        model = tf.keras.models.load_model('cnnmodel.hdf5')
+    else:
+        model = tf.keras.models.load_model('Model2.weights.hdf5')
     return model
 
-model = load_model()
 st.write("""# Sarcasm Detection System""")
-sentence_type = st.radio("Select sentence type:", ("Type 1", "Type 2"))
 sentence = st.text_input('Please enter a sentence.')
 
-if sentence is not None and sentence != "":
+model_type = st.selectbox('Select Model Type', ('Model 1', 'Model 2'))
+
+if sentence is None:
+    st.text("Please enter a sentence.")
+else:
     st.write('Inputted Sentence: ', sentence)
     predcorpus = [sentence]
-    onehot_ = [one_hot(words, voc_size) for words in predcorpus]
+    onehot_ = [one_hot(words, voc_size) for words in predcorpus] 
     embedded_docs = pad_sequences(onehot_, padding='pre', maxlen=max_sent_length)
     
-    # Get labels based on probability
-    predictions = model.predict(embedded_docs)
+    model = load_model(model_type)
+    
+    # Get labels based on probability 1 if p >= 0.01 else 0
+    prediction = model.predict(embedded_docs)
     pred_labels = []
-    for prediction in predictions:
-        if prediction >= 0.5:
-            pred_labels.append(1)
-        else:
-            pred_labels.append(0)
-    
-    if sentence_type == "Type 1":
-        if pred_labels[0] == 1:
-            output = 'Sarcasm Detected'
-        else:
-            output = 'No Sarcasm Detected'
-        prediction = str(predictions[0][0] * 100) + '%'
-        
-    elif sentence_type == "Type 2":
-        if pred_labels[0] == 1:
-            output = 'Positive Sentiment Detected'
-        else:
-            output = 'Negative Sentiment Detected'
-        prediction = str(predictions[0][0] * 100) + '%'
-    
+    if prediction >= 0.5:
+        pred_labels.append(1)
+    else:
+        pred_labels.append(0)
+    if pred_labels[0] == 1:
+        output = 'Sarcasm Detected'
+    else:
+        output = 'No Sarcasm Detected'
+    prediction = (str(prediction[0][0] * 100)) + '%'
     st.write("Prediction Accuracy: ", prediction)
     string = "OUTPUT: " + output
     st.success(string)
-else:
-    st.text("Please enter a sentence.")
